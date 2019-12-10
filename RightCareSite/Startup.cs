432d1f1131fd7df1df6c9565469f600478e845f7
AminGomaa <48386393+AminGomaa@users.noreply.github.com>
2019-12-10 23:39:56 +0200

    Add project files.

---
+++ RightCareSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RightCareSite.Startup))]
+namespace RightCareSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
